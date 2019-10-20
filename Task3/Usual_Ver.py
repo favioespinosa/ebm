e0c61108fa8ee631fb0d9545a4513371a62c6e2a
favioespinosa <55297611+favioespinosa@users.noreply.github.com>
2019-10-20 16:27:18 +0300

    Update Usual_Ver.py

--- Task3/Usual_Ver.py
+++ Task3/Usual_Ver.py
@@ -2,25 +2,26 @@ import os
 import shutil
 import threading
 import time
-import numpy
+import numpy as np
 import librosa as lr
 from glob import glob
-import conv
-def Reader(audio_files,files,f):
+from concurrent.futures import ProcessPoolExecutor
+def Reader(audio_files,files):
 	for i in range(len(audio_files)):
 		x, fs = lr.load(audio_files[i])
 		mfccs = lr.feature.mfcc(x, sr=fs)
-		f.write(files+': ',)
-		f.write(str(mfccs.shape)+'\n')
+		return str(mfccs.shape)
 def Organizador(path):#It organize to read the audio files
-	print("Joder")
-	f=open("mfccs_coeficients.txt",'w')
+	my_array=np.array([[" "],[" "]])
 	for root, subdirs, files in os.walk(path):
 		for i in range(len(files)):
 			audio_files=glob(root+'\\'+files[i])
-			Reader(audio_files,files[i],f)
-path=r'C:\Users\1\Documents\Python\Programacion_Paralela\Audio'
+			b=np.array([[str(Reader(audio_files,files[i]))],[root+'\\'+str(files[i])]])
+			my_array=np.concatenate((my_array,b))
+	return my_array
+path=r'C:\Users\1\Documents\Python\Programacion_Paralela\Audio2'
 t_start = time.time()
-Organizador(path)
+my_array=Organizador(path)
+#print(my_array)
 p=open("Readme.txt",'a')
-p.write(str(time.time() - t_start)+'\n')
+p.write("Secuence program:"+str(time.time() - t_start)+'\n')
